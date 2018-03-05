f10cea934811f2c5a28016623eaf9ba5e1e1868a
Caio Fabio de Araujo <caio.araujo@linx.com.br>
2018-03-05 16:53:56 -0300

    Updates

---
+++ src/main/kotlin/br/caiodearaujo/awssqstest/SqsListener.kt
@@ -0,0 +1,18 @@
+package br.caiodearaujo.awssqstest
+
+import org.apache.juli.logging.LogFactory
+import org.springframework.jms.annotation.JmsListener
+import org.springframework.stereotype.Service
+
+@Service
+class SqsListener {
+
+    internal val logger = LogFactory.getLog(this::class.java)
+
+            @JmsListener(destination = "TestSQS_001")
+            fun listenerSqs(request: String) {
+                this.logger.info("Received")
+                this.logger.info(request)
+            }
+
+}
